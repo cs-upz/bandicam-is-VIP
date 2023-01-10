c4c5f85f97b86e2ad63b53f55398270ba2c177ba
依托答辩的ikun <122326706+cs-upz@users.noreply.github.com>
2023-01-10 14:03:04 +0800

    Add files via upload

---
+++ App/Bandicam/RegVulkanLayer.bat
@@ -0,0 +1,2 @@
+%windir%\SysWOW64\rundll32.exe "%~dp0bdcamvk32.dll",RegDll
+%windir%\system32\rundll32.exe "%~dp0bdcamvk64.dll",RegDll
---
+++ App/Bandicam/UnregVulkanLayer.bat
@@ -0,0 +1,2 @@
+%windir%\SysWOW64\rundll32.exe "%~dp0bdcamvk32.dll",UnregDll
+%windir%\system32\rundll32.exe "%~dp0bdcamvk64.dll",UnregDll
